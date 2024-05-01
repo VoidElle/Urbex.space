6d407712769727bcc1a677385be7cbb835d52c14
Luca <lucadelc@gmail.com>
2024-05-01 15:34:55 +0200

    💅 Updated prettier rules

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,24 +1,25 @@
-"use client";
+"use client"
 
-import React from "react";
-import { Icons } from "./Icon";
-import useLoadingState, { LoadingState } from "@/states/loading-state";
+import React from "react"
+import { Icons } from "./Icon"
+import useLoadingState, { LoadingState } from "@/states/loading-state"
 
 export default function Loading(): React.JSX.Element {
-  const isLoading: boolean = useLoadingState(
-    (state: LoadingState) => state.loading,
-  );
-  if (!isLoading) {
-    return <></>;
-  }
+    const isLoading: boolean = useLoadingState(
+        (state: LoadingState) => state.loading
+    )
 
-  return (
-    <div
-      className={
-        "h-screen w-screen relative flex items-center justify-center bg-black opacity-80 z-50"
-      }
-    >
-      <Icons.spinner className="h-20 w-20 animate-spin z-50 text-white" />
-    </div>
-  );
+    if (!isLoading) {
+        return <></>
+    }
+
+    return (
+        <div
+            className={
+                "h-screen w-screen relative flex items-center justify-center bg-black opacity-80 z-50"
+            }
+        >
+            <Icons.spinner className="h-20 w-20 animate-spin z-50 text-white" />
+        </div>
+    )
 }
